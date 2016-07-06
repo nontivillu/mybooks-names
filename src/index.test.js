2a484dde3dfef5b4930aaa6155c51adfb258c0b4
Kent C. Dodds <kent@doddsfamily.us>
2016-07-06 07:14:42 -0600

    write tests 📝

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,19 @@
 const {expect} = require('chai')
+const starWarsNames = require('.')
 
 describe('starwars-names', () => {
-  it('should work', () => {
-    expect(true).to.be.true
+  it('should have a list of all available names', () => {
+    expect(starWarsNames.all).to.satisfy(isArrayOfStrings)
+  })
+  it('should allow me to get a random name from the list', () => {
+    expect(starWarsNames.random()).to.satisfy(isIncludedIn(starWarsNames.all))
   })
 })
+
+function isArrayOfStrings(array) {
+  return array.every(i => typeof i === 'string')
+}
+
+function isIncludedIn(array) {
+  return item => array.includes(item)
+}
